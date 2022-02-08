a1dbd299919572cf3f5847416a05318384e2af94
Amadeus <silentspiral@outlook.com>
2022-02-08 20:25:53 +0800

    mysql笔记提交

---
+++ MySQL/Code/day04.sql
@@ -0,0 +1,20 @@
+#第03章_基本select语句的课后练习
+SELECT * FROM employees;
+
+# 1.查询员工12个月的工资总和，并起别名为ANNUAL SALARY
+SELECT employee_id, first_name, last_name, salary * (1 + IFNULL(commission_pct, 0)) * 12 "ANNUAL SALARY"
+FROM employees;
+# 2.查询employees表中去除重复的job_id以后的数据
+SELECT DISTINCT job_id 
+FROM employees;
+# 3.查询工资大于12000的员工姓名和工资
+SELECT employee_id, first_name, last_name, salary
+FROM employees
+WHERE salary > 12000;
+# 4.查询员工号为176的员工的姓名和部门号
+SELECT first_name, last_name, department_id 
+FROM employees
+WHERE employee_id = 176;
+# 5.显示表 departments 的结构，并查询其中的全部数据
+
+#第四章·运算符
